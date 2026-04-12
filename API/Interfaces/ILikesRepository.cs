7e20f6a22a1e48a622e6e89c69606b764849501f
Anas Al Zoubi <alzoubi.anas.97@gmail.com>
2026-04-12 18:16:13 +0200

    End of section 14

---
+++ API/Interfaces/ILikesRepository.cs
@@ -0,0 +1,14 @@
+using API.Entities;
+using API.Helpers;
+
+namespace API.Interfaces;
+
+public interface ILikesRepository
+{
+    Task<MemberLike?> GetMemberLike(string sourceMemberId, string likedMemberId);
+    Task<PaginatedResult<Member>> GetMemberLikes(LikesParam likesParam);
+    Task<IReadOnlyList<string>> GetCurrentUserLikedMembersIds(string memberId);
+    void DeleteLike(MemberLike like);
+    void AddLike(MemberLike like);
+    Task<bool> SaveAllAsync();
+}
